4a59aeb4be9a10bf235779e86c455171d41cdb3e
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-10-09 18:50:37 +0530

    Created Forgot Password Route and Controller

--- server/Controllers/authControllers.js
+++ server/Controllers/authControllers.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const AppError = require("../Utils/appError");
 const { promisify } = require("util");
 const crypto = require('crypto')
+const sendEmail = require('./../Utils/email')
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -61,6 +62,48 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 
+exports.forgetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError("There is no user with email address.", 404));
+  }
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3) Send it to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Your password reset token (valid for 10 min)",
+      message,
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Token sent to email!",
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError("There was an error sending the email. Try again later!"),
+      500
+    );
+  }
+});
+
+
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
   let token;
@@ -105,8 +148,6 @@ exports.protect = catchAsync(async (req, res, next) => {
 
 
 exports.getMyProfile = catchAsync(async (req,res,next) => {
-  console.log(req.user);
-  console.log(req.user._id);
   const user = await User.findById(req.user._id);
   res.json({
     status: 'success',
--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
+router.post('/forgotpassword', authController.forgetPassword)
+// router.post('/resetToken/:token', authController)
 
 router.use(authController.protect)
 
