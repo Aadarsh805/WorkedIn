5862475cc8210c1d90abdca7fd4e9f2696a6d24a
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-10-30 14:41:31 +0530

    Fixed Email User's Firstname BUG 🐛

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -41,7 +41,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
   });
-  const url = `${req.protocol}://${req.get('host')}/me`
+  const url = `${req.protocol}://${req.get('host')}/api/v1/posts`
   await new Email(newUser, url).sendWelcome()
   createSendToken(newUser, 201, res);
 });
