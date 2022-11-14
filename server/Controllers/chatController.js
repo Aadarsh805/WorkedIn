e17a9c5eb22e7c714b95bab5ddd0226f91b606ad
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-11-15 01:54:41 +0530

    Created Access to One-on-One Chat

--- server/Controllers/chatController.js
+++ server/Controllers/chatController.js
@@ -4,7 +4,50 @@ const catchAsync = require("../Utils/catchAsync");
 const AppError = require("../Utils/appError");
 
 exports.accessChat = catchAsync(async (req, res) => {
-  res.send("Chat Initiated");
+  const { userId } = req.body;
+
+  if (!userId) {
+    return next(new AppError('UserId param not sent with request'))
+  }
+
+  var isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate("users", "name")
+    .populate("latestMessage");
+
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
+  });
+
+  if (isChat.length > 0) {
+    res.status(200).json({
+      status: 'success',
+      chat: isChat[0]
+    });
+  } else {
+    var chatData = {
+      chatName: "one_On_one",
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    };
+
+    const createdChat = await Chat.create(chatData);
+    const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+    );
+
+    res.status(200).json({
+      status: 'success',
+      chat: FullChat
+    });
+  }
 });
 
 exports.fetchChats = catchAsync(async (req, res) => {
