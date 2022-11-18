84de4bb8ab78eccbaddd1318566d516c6edd9826
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-11-18 13:29:38 +0530

    Message GET and POST APi Created 🤩

--- server/Controllers/messageController.js
+++ server/Controllers/messageController.js
@@ -1,12 +1,49 @@
 const Message = require("../Models/messageModel");
 const User = require("../Models/userModel");
 const Chat = require("../Models/chatModel");
-const catchAsync = require('../Utils/catchAsync')
+const catchAsync = require('../Utils/catchAsync');
+const AppError = require("../Utils/appError");
 
 exports.allMessages = catchAsync(async (req,res) => {
-    res.send('Hello') 
+    // console.log(req.params);
+    const chatId = req.params.id
+    const messages = await Message.find({ chat: chatId })
+    .populate("sender", "name photo")
+      .populate("chat");
+
+    res.status(200).json({
+        status: 'success',
+        msgs: messages
+    });
 })
 
-exports.sendMessage = catchAsync(async (req,res) => {
-    res.send('SendMessage')
+exports.sendMessage = catchAsync(async (req,res,next) => {
+    const chatId = req.params.id;
+    const content = req.body.chatMessage;
+    
+    if (!content || !chatId ) {
+        return next(new AppError('Invalid data passed into request'))
+    }
+
+    var newMessage = {
+        sender: req.user.id,
+        content: content,
+        chat: chatId,
+    };
+
+    var message = await Message.create(newMessage);
+
+    message = await message.populate("sender", "name photo");
+    message = await message.populate("chat", "chatName");
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name",
+    });
+
+    await Chat.findByIdAndUpdate(chatId, { latestMessage: message });
+
+    res.json({
+        status: 'success',
+        msgs: message
+    })
 })
--- server/Routes/messageRoutes.js
+++ server/Routes/messageRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const messageController = require('../Controllers/messageController')
 const authController = require('../Controllers/authController')
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
     .route("/")
