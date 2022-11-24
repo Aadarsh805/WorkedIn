b1656302aa0d9fd9b4e73c41cc5de3184a0f9151
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-11-24 19:07:54 +0530

    Created axios helper function and specified API Routes

---
+++ client/src/Utils/APIRoutes.ts
@@ -0,0 +1,35 @@
+export const BASE_URL = "http://localhost:5000";
+
+export const userEnd = "/api/v1/users"
+export const postEnd = "/api/v1/posts"
+export const chatEnd = "/api/v1/chats"
+export const contractEnd = "/api/v1/contracts"
+export const commentEnd = "/comment";
+export const messageEnd = "/messages"
+
+// auth routes
+export const signup = "/signup"
+export const login = "/login"
+export const forgetPassword = "/forgotpassword";
+export const resetPassword = "/resetPassword/"
+
+//  user routes
+export const searchUser = "/"
+export const getMyProfile = "/me"
+
+// post routes
+export const likePost = "/like"
+
+// comment routes
+
+// chat routes
+export const createGroupChat = "/group"
+export const renameGroupChat = "/rename"
+export const removeFromGroupChat = "/groupremove"
+export const addToGroupChat = "/groupadd"
+
+// message routes
+
+// contract routes
+export const acceptContract = "/accept";
+export const denyContract = "/deny"
